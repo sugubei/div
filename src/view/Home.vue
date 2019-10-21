7cf7bf3b4ce930644bc8be2275adbdb0e728be5c
sugubei <2223065621@qq.com>
2019-10-21 12:34:58 +0800

    另外6个div

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -10,6 +10,24 @@
       <teacup-loading />
     </div>
     <div>
+      <hert />
+    </div>
+    <div>
+      <land-mark />
+    </div>
+    <div>
+      <badge />
+    </div>
+    <div>
+      <calendar />
+    </div>
+    <div>
+      <mail />
+    </div>
+    <div>
+      <vue />
+    </div>
+    <div>
       <play-button />
     </div>
     <div>
@@ -48,10 +66,10 @@ export default {
 .home {
   display: grid;
   grid-template-columns: repeat(
-    3,
+    6,
     1fr
   ); //网格列数 重复创建3个 1rf 的列 1rf是总空间减去固定空间和百分比大小，然后再分配
-  grid-template-rows: repeat(2, 1fr); // 网格行数 重复创建 2个 1rf的行
+  grid-template-rows: repeat(3, 1fr); // 网格行数 重复创建 2个 1rf的行
   height: 100vh;
   justify-items: center;
   align-items: center;
