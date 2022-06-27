8f23bc1ab8d7691586677cbfec4539635f5a0d25
sunhz <2223065621@qq.com>
2022-06-27 16:12:29 +0800

    update: vue3

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <HelloWorld />
+</template>
+
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+a {
+  text-decoration: inherit;
+}
+em {
+  font-style: normal;
+}
+ul,
+ol {
+  list-style: none;
+}
+h1,
+h2,
+h3,
+h4 {
+  font: inherit;
+}
+input,
+select,
+button {
+  font: inherit;
+}
+html,
+body {
+  height: 100%;
+}
+body {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  background-image: linear-gradient(45deg, #f4f1ff, #fff5f5);
+}
+#app {
+  height: 100%;
+  overflow: hidden;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="home">
+    <div>
+      <circle-loading />
+    </div>
+    <div>
+      <bubble-loading />
+    </div>
+    <div>
+      <teacup-loading />
+    </div>
+    <div>
+      <hert />
+    </div>
+    <div>
+      <land-mark />
+    </div>
+    <div>
+      <badge />
+    </div>
+    <div>
+      <calendar />
+    </div>
+    <div>
+      <mail />
+    </div>
+    <div>
+      <vue />
+    </div>
+    <div>
+      <play-button />
+    </div>
+    <div>
+      <progress-bar />
+    </div>
+    <div>
+      <floating-button />
+    </div>
+  </div>
+</template>
+<style lang="less" scoped>
+.home {
+  display: grid;
+  grid-template-columns: repeat(
+    6,
+    1fr
+  ); /*//网格列数 重复创建3个 1rf 的列 1rf是总空间减去固定空间和百分比大小，然后再分配
+*/
+  grid-template-rows: repeat(3, 1fr); /*// 网格行数 重复创建 2个 1rf的行
+*/
+  height: 100vh;
+  justify-items: center;
+  align-items: center;
+  max-width: 800px;
+  margin: 0 auto;
+}
+.home > div {
+  transform: scale(1.5);
+}
+</style>
