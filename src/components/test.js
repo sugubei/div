0ca0d7262e0950c06a1ad08d2b6e30c9045049b5
sugubei <33868383+sugubei@users.noreply.github.com>
2022-06-29 14:40:50 +0800

    Delete test.js

--- src/components/test.js
+++
@@ -1,7 +0,0 @@
-const path = require('path')
-const files = ['./BubbleLoading.vue', './xxx.vue', './Bubble.vue']
-
-files.forEach(key => {
-  const name = path.basename(key, '.vue')
-  console.log(name)
-})
